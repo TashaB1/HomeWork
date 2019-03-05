accf7c34701839713d6e3a7827ed8bcbc55a1263
TashaB1 <tasha2k7@mail.ru>
2019-03-06 00:22:30 +0300

    create new branch "feature_4", add new logic

--- app/src/main/java/com/tashad16a/gmail/homework/MainActivity.kt
+++ app/src/main/java/com/tashad16a/gmail/homework/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.tashad16a.gmail.homework
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-
-import kotlinx.android.synthetic.main.activity_main.*
 import android.view.Gravity
 import android.view.View
 import android.widget.ImageView
@@ -19,17 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
-    fun showToastHello(view: View){
+    fun showToastHello(view: View) {
         val toast = Toast.makeText(
-            this,
+            applicationContext,
             "Hello world! Have a nice evening!",
             Toast.LENGTH_LONG
         )
@@ -41,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         toast.show()
     }
 
-    fun showToastBye(view: View){
+    fun showToastBye(view: View) {
         val toast = Toast.makeText(
             applicationContext,
             "Goodbye world! Good dreams!",
@@ -53,22 +42,19 @@ class MainActivity : AppCompatActivity() {
         pic.setImageResource(R.drawable.sunset)
         toastView.addView(pic, 0)
         toast.show()
-
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    fun showToastHelloMyDream(view: View) {
+        val toast = Toast.makeText(
+            applicationContext,
+            "Hello my dream job!",
+            Toast.LENGTH_LONG
+        )
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        val toastView = toast.view as LinearLayout
+        val pic = ImageView(applicationContext)
+        pic.setImageResource(R.drawable.work)
+        toastView.addView(pic, 0)
+        toast.show()
     }
 }
