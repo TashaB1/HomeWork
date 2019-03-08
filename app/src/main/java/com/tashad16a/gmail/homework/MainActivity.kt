bd588ee63a308df1a1599232184df2bdf29bd0ba
TashaB1 <tasha2k7@mail.ru>
2019-03-08 14:38:24 +0300

    clean code

--- app/src/main/java/com/tashad16a/gmail/homework/MainActivity.kt
+++ app/src/main/java/com/tashad16a/gmail/homework/MainActivity.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,44 +16,62 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun showToastHello(view: View) {
-        val toast = Toast.makeText(
+        val toast: Toast?
+        val toastView: LinearLayout
+        val pic: ImageView
+
+        toast = Toast.makeText(
             applicationContext,
             "Hello world! Have a nice evening!",
             Toast.LENGTH_LONG
         )
         toast.setGravity(Gravity.CENTER, 0, 0);
-        val toastView = toast.view as LinearLayout
-        val pic = ImageView(applicationContext)
-        pic.setImageResource(R.drawable.morning)
+
+        toastView = toast.view as LinearLayout
+        pic = ImageView(applicationContext)
+        pic.setImageResource(R.drawable.ic_morning)
         toastView.addView(pic, 0)
+
         toast.show()
     }
 
     fun showToastBye(view: View) {
-        val toast = Toast.makeText(
+        val toast: Toast?
+        val toastView: LinearLayout
+        val pic: ImageView
+
+        toast = Toast.makeText(
             applicationContext,
             "Goodbye world! Good dreams!",
             Toast.LENGTH_LONG
         )
         toast.setGravity(Gravity.CENTER, 0, 0);
-        val toastView = toast.view as LinearLayout
-        val pic = ImageView(applicationContext)
-        pic.setImageResource(R.drawable.sunset)
+
+        toastView = toast.view as LinearLayout
+        pic = ImageView(applicationContext)
+        pic.setImageResource(R.drawable.ic_sunset)
         toastView.addView(pic, 0)
+
         toast.show()
     }
 
     fun showToastHelloMyDream(view: View) {
-        val toast = Toast.makeText(
+        val toast: Toast?
+        val toastView: LinearLayout
+        val pic: ImageView
+
+        toast = Toast.makeText(
             applicationContext,
             "Hello my dream job!",
             Toast.LENGTH_LONG
         )
         toast.setGravity(Gravity.CENTER, 0, 0);
-        val toastView = toast.view as LinearLayout
-        val pic = ImageView(applicationContext)
-        pic.setImageResource(R.drawable.work)
+
+        toastView = toast.view as LinearLayout
+        pic = ImageView(applicationContext)
+        pic.setImageResource(R.drawable.ic_work)
         toastView.addView(pic, 0)
+
         toast.show()
     }
 }
