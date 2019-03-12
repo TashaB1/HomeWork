f501cff0e3444f072f067032b8b019c4a7654baa
TashaB1 <tasha2k7@mail.ru>
2019-03-13 01:59:12 +0300

    add navigition activity add page frofile vk (TODO)

---
+++ hw0603uitask2/src/main/java/com/tashad16a/gmail/hw0603ui/uigooglepodcasts/GooglePodcastsActivity.java
@@ -0,0 +1,15 @@
+package com.tashad16a.gmail.hw0603ui.uigooglepodcasts;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.tashad16a.gmail.hw0603_ui.R;
+
+public class GooglePodcastsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_google_podcasts);
+    }
+}
