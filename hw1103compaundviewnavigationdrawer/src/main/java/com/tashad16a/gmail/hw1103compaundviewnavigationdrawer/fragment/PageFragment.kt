8ff146febc25fdbe6a87b56f75a8d604b2967d6f
TashaB1 <tasha2k7@mail.ru>
2019-04-10 00:48:37 +0300

    Revert "add annotations to the method onCreateView class PageFragment"
    This reverts commit c672c7f320ed8e9d97bdc145e71cf5a7be967953.

--- hw1103compaundviewnavigationdrawer/src/main/java/com/tashad16a/gmail/hw1103compaundviewnavigationdrawer/fragment/PageFragment.kt
+++ hw1103compaundviewnavigationdrawer/src/main/java/com/tashad16a/gmail/hw1103compaundviewnavigationdrawer/fragment/PageFragment.kt
@@ -1,19 +1,16 @@
 package com.tashad16a.gmail.hw1103compaundviewnavigationdrawer.fragment
 
 import android.os.Bundle
-import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tashad16a.gmail.hw1103compaundviewnavigationdrawer.R
-import org.jetbrains.annotations.NotNull
 
 class PageFragment : Fragment() {
 
-    @Nullable
     override fun onCreateView(
-        @NotNull inflater: LayoutInflater,
+        inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
